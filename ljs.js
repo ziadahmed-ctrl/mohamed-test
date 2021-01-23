d56f530eac6c3273ec3ed5fb08485de0657e96b5
ziad ahmed <ziadtiger5@gmail.com>
2021-01-23 10:25:39 +0200

    frist commit

---
+++ ljs.js
@@ -0,0 +1,18 @@
+/*age=15;
+aGe=20+1-1;
+rules='this site use cookies';
+alert(aGe+age);
+alert(rules)
+var d = 500,
+//ARRAY
+e=["username","age"]
+document.getElementById("text").innerHTML = e[1] + d;
+//object
+var mansoor={username:"gamed",age:"15"};
+document.getElementById("text").innerHTML = mansoor.age; */
+
+var tram=7;
+var tram5=7;
+document.getElementById("text").innerHTML =
+myAgeIs
+ tram * tram5
